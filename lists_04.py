605cdf789f6179f89b956871d37702e643b71ca2
williamboco21 <williammboco@gmail.com>
2020-10-27 13:42:41 +0800

    changes

--- lists_04.py
+++ lists_04.py
@@ -6,3 +6,12 @@ print(abc)
 print(my_list)
 print(len(my_list))
 print(my_list[0])
+
+# Another exercise using the split() function
+phrase = "From stephen.marquard@uct.ac.za Sat Jan 5 09:14:16 2008"
+for line in phrase:
+    line = line.rstrip()
+    if not line.startswith('From '):
+        continue
+    words = line.split()
+    print(words[2])
